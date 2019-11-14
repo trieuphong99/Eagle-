cac782e4ba44e8893870abc041239746118dec64
Trieu Phong <trieuphong328@gmail.com>
2019-11-14 09:18:56 +0700

    delete redundant files

--- Eagle/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.http import HttpResponse
-from .forms import *
-
-# Create your views here.
-def home(request):
-    return render(request, 'home/home_base.html')
-
